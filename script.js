e8e8f4be5973e3d334ed7da30558fb12ccabc026
Ryann908 <rysuero@gmail.com>
2025-09-30 14:51:14 -0400

    Added imgs, more html, added to home page

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-const menu = document.querySelector("menu");
-const pres = document.getElementById("displayNo");
+const presentation = document.getElementById("presentation");
+const deliverables = document.getElementById("deliverables");
+const prototype = document.getElementById("prototype");
+
+const displayerDel = document.getElementById("displayNoDel");
+const displayerPre = document.getElementById("displayNoPre");
+const displayerPro = document.getElementById("displayNoPro");
 
 
-menu.addEventListener(('click'), () => {
-    if(pres.getAttribute("id") == "displayNo"){
-        pres.className = "display";
-        pres.removeAttribute("id");
-    } else{
-        pres.id = "displayNo";
-        pres.removeAttribute("class");
-    }
+presentation.addEventListener(('mouseenter'), () => {
+        displayerPre.className = "display";
+        displayerPre.removeAttribute("id");
 });
+
+presentation.addEventListener(('mouseleave'), () => {
+        displayerPre.id = "displayNoPre";
+        displayerPre.removeAttribute("class");
+
+});
+
+
+deliverables.addEventListener(('mouseenter'), () => {
+        displayerDel.className = "display";
+        displayerDel.removeAttribute("id");
+});
+
+deliverables.addEventListener(('mouseleave'), () => {
+        displayerDel.id = "displayNoDel";
+        displayerDel.removeAttribute("class");
+
+});
+
+
+prototype.addEventListener(('mouseenter'), () => {
+        displayerPro.className = "display";
+        displayerPro.removeAttribute("id");
+});
+
+prototype.addEventListener(('mouseleave'), () => {
+        displayerPro.id = "displayNoPro";
+        displayerPro.removeAttribute("class");
+
+});
+
+
+
+
+
+
+
+
+// https://stockcake.com/i/organized-pantry-shelves_1155144_917047
